a4bc82c778dcedd6f97241875c00fe7dce067b49
Yash <yashdariyal31@gmail.com>
2024-09-08 10:40:30 +0000

    conversion in js

--- basics/test.js
+++ basics/test.js
@@ -13,7 +13,19 @@
 // we cannot use alert(3+3) here as it is a node js code//it can run in the browser
 //console.log(typeof king)
 //console.log(typeof King)// NULL HAS A TYPEOF OBJECT BUT UNDEFINED VALUE HAS A TYPEOF UNDEFINED ONLY//
-
-let n = "33"
+let n = "33abc"
  let valueinNumber = Number(n);
- console.log(typeof n)
+ console.log( typeof valueinNumber)  
+ console.log(valueinNumber)
+  // for a vlue of 33 => number 33
+  //for "33abc" => Nan
+  //for null => 0
+  //for true => 1
+  //false=>0
+  let booleanloggedin = "null"
+  let booleanvalue =Boolean(booleanloggedin)
+  console.log(booleanvalue)
+  
+// when we give boolean 1 it returned us true//
+//when no value "" =>false
+//whenn given a string it returned us true//
