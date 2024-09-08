af8141978bab5d0ed23d0c4d6d0a8fa2bdf1a30f
Yash <yashdariyal31@gmail.com>
2024-09-08 04:52:12 +0000

    Introduction to js

---
+++ basics/test.js
@@ -0,0 +1,19 @@
+//console.log("hello naman")
+//const king ="rome"
+//let King = null
+//let password = 12345;{
+
+
+//let King ="Rose"
+//console.table([king,King,password]) // used for printing multiple items at a time //
+
+//}
+//"use Strict" treats all the java version as new 
+
+// we cannot use alert(3+3) here as it is a node js code//it can run in the browser
+//console.log(typeof king)
+//console.log(typeof King)// NULL HAS A TYPEOF OBJECT BUT UNDEFINED VALUE HAS A TYPEOF UNDEFINED ONLY//
+
+let n = "33"
+ let valueinNumber = Number(n);
+ console.log(typeof n)
